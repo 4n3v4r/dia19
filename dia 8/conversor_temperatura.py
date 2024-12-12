4266109b3d0b2d1d0995165552ffade2935614f1
4n3v4r <spqrome@gmail.com>
2024-12-12 14:40:07 +0000

    dia8

---
+++ dia 8/conversor_temperatura.py
@@ -0,0 +1,15 @@
+# conversor temperatura
+import math
+def celsius_farenheit(celsius):
+    farenheit = (celsius * 9/5) + 32
+    return farenheit
+
+def farenheit_celsius(farenheit):
+    celsius = (farenheit - 32) * 5/9
+    return round(celsius, 2)
+
+celsius = float(input("Digite o valor em Celsius: "))
+print(f"O valor em Farenheit é: {celsius_farenheit(celsius)} °F")
+
+farenheit = float(input("Digite o valor em Farenheit: "))
+print(f"O valor em Celsius é: {farenheit_celsius(farenheit)} °C")
