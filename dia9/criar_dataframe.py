6731bef644bc1808129d9a924350af941eccada1
4n3v4r <spqrome@gmail.com>
2024-12-14 14:22:13 +0000

    criar_dataframe.py

--- dia9/criar_dataframe.py
+++ dia9/criar_dataframe.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import numpy as np 
 
-data = {vendas: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],}
+data = {'vendas': [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],}
 df = pd.DataFrame(data) #criando o dataframe
 
 #operações
-media = df[vendas].mean() #media
-mediana = df[vendas].median() #mediana
-desvio_padrao = df[vendas].std() #desvio padrão
-maximo = df[vendas].max() #maior valor
-minimo = df[vendas].min() #menor valor
+media = df['vendas'].mean() #media
+mediana = df['vendas'].median() #mediana
+desvio_padrao = df['vendas'].std() #desvio padrão
+maximo = df['vendas'].max() #maior valor
+minimo = df['vendas'].min() #menor valor
 
 #resultados
 print("Media: ", media)
